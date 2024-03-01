517c2721d642af27e5c0f5071d864e5250b53230
Irshad <irshad365@gmail.com>
2024-03-01 18:55:17 -0500

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:expense_tracker/app.dart';
+import 'package:expense_tracker/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
